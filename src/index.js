de32a3edfd0425e0f759c5567394f9c4532b9566
JairoChoconta <jalexanderchoconta97@gmail.com>
2022-11-19 22:53:51 -0500

    proyecto uno DOM, finalizado.

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const baseUrl = "https://platzi-avo.vercel.app";
 const appNode = document.querySelector("#app");
 
 appNode.addEventListener("click", (event) => {
-    if(event.target.nodeName === 'H2') {
+  if (event.target.nodeName === "H2") {
     window.alert("Hola!");
-    }
+  }
 });
 
 const formatPrice = (price) => {
@@ -40,18 +40,29 @@ window
     responseJson.data.forEach((item) => {
       //console.log(item.name);
       // crear imagen
-      const image = document.createElement("img");
-      image.src = `${baseUrl}${item.image}`;
+      const imagen = document.createElement("img");
+      imagen.src = `${baseUrl}${item.image}`;
+      imagen.className = "h-32 w-32 md:h-24 md:2-24 rounded-full mx-auto md:mx-0 md:mr-6";
       // crear titulo
       const title = document.createElement("h2");
       title.textContent = item.name;
-      title.className = "text-2xl text-red-600";
+      title.className = "text-2xl font-bold text-green-500";
       // crear precio
       const price = document.createElement("div");
-      price.className = "text-gray-600";
+      price.className = "font-medium text-center text-xl";
       price.textContent = formatPrice(item.price);
+      // Crear contenedor para el título y el precio
+      const cardInfo = document.createElement("div");
+      cardInfo.className = "text-center md:text-left";
+      cardInfo.append(title, price);
+      // Meter todo dentro de una card
+      const card = document.createElement("div");
+      card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+      card.append(imagen, cardInfo);
+
+      // Metemos todo dentro del contenedor principal
       const container = document.createElement("div");
-      container.append(image, title, price);
+      container.append(card);
 
       todosLosItems.push(container);
     });
